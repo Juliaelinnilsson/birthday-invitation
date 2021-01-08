c06e839b6ac29dcfbb6436a188c48ef3490533a6
Julia Nilsson <nilsson.juliaelin@gmail.com>
2021-01-08 12:36:33 +0100

    Patch: Vue set up

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <RegistrationUser msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RegistrationUser from '@/components/RegistrationUser.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    RegistrationUser
+  }
+}
+</script>
